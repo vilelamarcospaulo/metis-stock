7e27cb56cce71314212329a292070d299d553569
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2023-07-23 19:05:38 -0300

    feat: execution of assets based on executors module

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/vilelamarcospaulo/metis/internal/status_invest"
+	"github.com/vilelamarcospaulo/metis/pkg/evaluator"
+	"github.com/vilelamarcospaulo/metis/pkg/rule"
+	"github.com/vilelamarcospaulo/metis/pkg/stock"
 )
 
 func init() {
@@ -13,5 +16,18 @@ func main() {
 	logrus.Info("Starting Metis")
 
 	dataProvider := status_invest.StatusInvestDataProvider{}
-	dataProvider.GetHistoricalData("PETR4")
+
+	// TODO :: Retrieve assets and rules from user wallet and preferences
+	assets := []stock.Asset{
+		stock.NewAsset(0, "SANB4", "Santander"),
+	}
+	rules := []rule.Rule{
+		rule.RoeGreaterThan(),
+	}
+
+	evaluator := evaluator.NewEvaluator(dataProvider, rules)
+	result := evaluator.Evaluate(assets)
+
+	// TODO :: Report module
+	logrus.Info(result)
 }
